129782bc294dbb59239c79a6607c1b9a005d466e
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-04-10 03:59:48 +0000

    áll

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""Basic Flask app
-"""
+'''Basic Flask app
+'''
 
 from flask import Flask, render_template
 
@@ -9,10 +9,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """html template"""
+    '''default route'''
     return render_template("0-index.html",)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-~                          
+    app.run(debug=True) 
