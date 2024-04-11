402e5cfbbc8aca94e57f4acba69196170ec2ae28
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-04-11 10:30:54 +0000

    all

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,46 +1,50 @@
 #!/usr/bin/env python3
-'''Force locale with URL parameter
-'''
-
+"""Force locale with URL parameter
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
 
 class Config:
-    '''Config class'''
-
-    DEBUG = True
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
-
-    Returns:
-        str: best match
+    """Retrives the locale for a web page
     """
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    # Get the locale parameter from the incoming request
+    locale = request.args.get('locale', None)
+    # Get list of supported languages from Config
+    supported_languages = app.config["LANGUAGES"]
+    if locale and locale in supported_languages:
+        # If the locale parameter is present and is a supported locale,
+        # return it
         return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    else:
+        # Use request.accept_languages to get the best match
+        best_match = request.accept_languages.best_match(supported_languages)
+        return best_match
 
 
-@app.route('/')
-def index() -> str:
-    '''
-    html template
-    '''
+@app.route("/")
+def index_4() -> str:
+    """The index page/The home
+    """
     return render_template("4-index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
