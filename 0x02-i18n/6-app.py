98e59d034143102f90afb04fd427dbb62430305e
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-04-12 20:47:08 +0000

    all

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
-"""Module for task 6
-"""
-from typing import Dict, Union
-from flask import Flask, render_template, request, g
+""" Route module for the API - Use user locale"""
+
+
+from flask import Flask, request, render_template, g
 from flask_babel import Babel
+from os import getenv
+from typing import Union
 
-app = Flask(__name__)
-
-app.url_map.strict_slashes = False
-
-
-class Config:
-    """Represents a Flask Babel configuration.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-babel = Babel(app)
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -27,76 +14,65 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+app = Flask(__name__)
+babel = Babel(app)
 
-@app.route("/")
-def index_6() -> str:
-    """The index function displays the home page of the web application.
 
-    Returns:
-        str: contents of the home page.
+class Config(object):
+    """ Setup - Babel configuration """
+    LANGUAGES = ['en', 'fr']
+    # these are the inherent defaults just btw
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# set the above class object as the configuration for the app
+app.config.from_object('6-app.Config')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return: 6-index.html
     """
-    return render_template("6-index.html")
+    return render_template('6-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """"Returns the preferred locale for the user.
-
-    The order of priority is:
-    1. Locale from URL parameters
-    2. Locale from user settings
-    3. Locale from request header
-    4. Default locale
-
-    Returns:
-        str: The preferred locale.
-    """
-    # Get the locale parameter from the incoming request
-    locale = request.args.get('locale')
-    # Get list of supported languages from Config
-    supported_languages = app.config["LANGUAGES"]
-    if locale and locale in supported_languages:
-        # If the locale parameter is present and is a supported locale,
-        # return it
-        return locale
-    # Locale from user settings
-    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
-        return g.user['locale']
-    header_locale = request.headers.get('locale', '')
-    if header_locale in app.config["LANGUAGES"]:
-        return header_locale
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    """ Determines best match for supported languages """
+    # check if there is a locale parameter/query string
+    if request.args.get('locale'):
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+    # check if there is a locale in an existing user's profile
+    elif g.user and g.user.get('locale')\
+            and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    # default to return as a failsafe
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-def get_user() -> Union[Dict, None]:
-    """Returns a user dictionary based on the given ID
-
-    Returns:
-        Union[Dict, None]: The user dictionary if found, otherwise None.
-    """
-    # Get the user_id from the login_as URL parameters
-    login_id = request.args.get('login_as')
-    # If the user ID exists in the URL parameters
-    if login_id:
-        # Get the user dictionary from the `users` dictionary using the user ID
-        return users.get(int(login_id))
-    # If the user ID does not exist in the URL parameters, return None
-    return None
+def get_user() -> Union[dict, None]:
+    """ Returns user dict if ID can be found """
+    if request.args.get('login_as'):
+        # have to type cast  the param to be able to search the user dict
+        user = int(request.args.get('login_as'))
+        if user in users:
+            return users.get(user)
+    else:
+        return None
 
 
 @app.before_request
-def before_request() -> None:
-    """Function to be executed before every request.
-    """
-    # Use the get_user function to get the user details
-    user = get_user()
-    # Set the user as a global variable on flask.g
-    g.user = user
-
-
-# Register the before_request function to be executed before every request
-app.before_request(before_request)
+def before_request():
+    """ Finds user and sets as global on flask.g.user """
+    g.user = get_user()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
